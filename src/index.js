cb224865f12aeda47606cd42377f03327b07c002
Diego Pedrozo <62295810+Diego-Pedrozo@users.noreply.github.com>
2023-09-19 11:20:45 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ app.use(express.json());
 //routes
 app.use(require('./routes/API'));
 
+app.get('/', (req, res) => {
+    res.send('API SaveLocation')
+})
+
 const port = process.env.port || 3000;
 app.listen(port, () => console.log(`http://localhost:${port}`));
 
